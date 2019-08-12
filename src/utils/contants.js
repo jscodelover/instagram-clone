ebb5ff5e4a3ddb891f5eabec14dff792e9c79163
jscodelover <manisha.4918@gmail.com>
2019-08-12 19:38:18 +0530

    feat: contants for images path

---
+++ src/utils/contants.js
@@ -0,0 +1,21 @@
+const CONTANTS = {
+	back: require('./img/back.png'),
+	camera: require('./img/camera.png'),
+	comment: require('./img/comment.png'),
+	fillHeart: require('./img/fillHeart.png'),
+	gridView: require('./img/gridView.png'),
+	heart: require('./img/heart.png'),
+	home: require('./img/home.png'),
+	info: require('./img/info.png'),
+	listView: require('./img/listView.png'),
+	message: require('./img/message.png'),
+	more: require('./img/more.png'),
+	redHeart: require('./img/redHeart.png'),
+	search: require('./img/search.png'),
+	setting: require('./img/setting.png'),
+	share: require('./img/share.png'),
+	tag: require('./img/tag.png'),
+	user: require('./img/user.png')
+};
+
+export default CONTANTS;
