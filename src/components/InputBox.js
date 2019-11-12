6eecb8ba1c49763b82c83b4d84d655498bfe1200
jscodelover <manisha.4918@gmail.com>
2019-11-12 13:48:12 +0530

    update firebase package

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
 
 function InputBox(props) {
-	const {
-		type,
-		style,
-		val,
-		placeholder,
-		autoCapitalize,
-		placeholderTextColor,
-		autoCorrect,
-		returnKeyType,
-		textContentType,
-		secureTextEntry,
-		handlechange,
-		keyboardType
-	} = props;
-	switch (type) {
-		case 1:
-			return (
-				<TextInput
-					style={[styles.inputField, style]}
-					onChangeText={text => handlechange(text)}
-					value={val}
-					placeholder={placeholder}
-					placeholderTextColor={placeholderTextColor || 'rgb(130, 130, 130)'}
-					autoCapitalize={autoCapitalize}
-					autoCorrect={autoCorrect}
-					returnKeyType={returnKeyType}
-					textContentType={textContentType || 'none'}
-					secureTextEntry={secureTextEntry}
-					keyboardType={keyboardType || 'default'}
-				/>
-			);
-		case 2:
-			return (
-				<TextInput
-					style={[styles.plainField, style]}
-					onChangeText={text => handlechange(text)}
-					value={val}
-					placeholder={placeholder}
-					placeholderTextColor={placeholderTextColor || 'rgb(130, 130, 130)'}
-					autoCapitalize={autoCapitalize}
-					autoCorrect={autoCorrect}
-					returnKeyType={returnKeyType}
-					textContentType={textContentType || 'none'}
-					secureTextEntry={secureTextEntry}
-					keyboardType={keyboardType || 'default'}
-				/>
-			);
-	}
+  const {
+    type,
+    style,
+    val,
+    placeholder,
+    autoCapitalize,
+    placeholderTextColor,
+    autoCorrect,
+    returnKeyType,
+    textContentType,
+    secureTextEntry,
+    handlechange,
+    keyboardType
+  } = props;
+  switch (type) {
+    case 1:
+      return (
+        <TextInput
+          style={[styles.inputField, style]}
+          onChangeText={text => handlechange(text)}
+          value={val}
+          placeholder={placeholder}
+          placeholderTextColor={placeholderTextColor || "rgb(130, 130, 130)"}
+          autoCapitalize={autoCapitalize}
+          autoCorrect={autoCorrect}
+          returnKeyType={returnKeyType}
+          textContentType={textContentType || "none"}
+          secureTextEntry={secureTextEntry}
+          keyboardType={keyboardType || "default"}
+        />
+      );
+    case 2:
+      return (
+        <TextInput
+          style={[styles.plainField, style]}
+          onChangeText={text => handlechange(text)}
+          value={val}
+          placeholder={placeholder}
+          placeholderTextColor={placeholderTextColor || "rgb(130, 130, 130)"}
+          autoCapitalize={autoCapitalize}
+          autoCorrect={autoCorrect}
+          returnKeyType={returnKeyType}
+          textContentType={textContentType || "none"}
+          secureTextEntry={secureTextEntry}
+          keyboardType={keyboardType || "default"}
+        />
+      );
+  }
 }
 
 const styles = StyleSheet.create({
-	inputField: {
-		borderColor: 'rgb(150, 150, 150)',
-		borderWidth: 1,
-		borderRadius: 5,
-		backgroundColor: 'rgb(248, 248, 248)',
-		width: '90%',
-		marginBottom: 15,
-		fontSize: 16,
-		paddingVertical: 13,
-		paddingHorizontal: 20,
-		height: 50
-	},
-	plainField: {
-		borderWidth: 0,
-		fontSize: 16,
-		width: '80%'
-	}
+  inputField: {
+    borderColor: "rgb(150, 150, 150)",
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: "rgb(248, 248, 248)",
+    width: "90%",
+    marginBottom: 15,
+    fontSize: 16,
+    paddingVertical: 13,
+    paddingHorizontal: 20,
+    height: 50
+  },
+  plainField: {
+    borderWidth: 0,
+    fontSize: 16,
+    width: "85%"
+  }
 });
 
 export default InputBox;
